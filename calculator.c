ce4af9aebff5ebd19fb681e1566246b214e867ef
abhi3naya <abhinayasharon5@gmail.com>
2025-03-11 14:24:22 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-int main() {
+int main()
+{
    char Operator;
    float num1, num2, result = 0;
    printf(" Enter any one operator like +, -, *, / : ");
    scanf("%c", &Operator);
    printf("Enter the values of Operands num1 and num2  : ");
    scanf("%f%f", &num1, &num2);
-   switch (Operator) {
+   switch (Operator) 
+ {
       case '+':
          result = num1 + num2;
          break;
@@ -19,4 +21,4 @@ int main() {
    }
    printf("The value = %f", result);
    return 0;
-}}
+}
